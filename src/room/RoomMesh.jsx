be8f3da5da92eb186b206fa91634be8ee30a1215
Karin Knutsson <karinmiriam@gmail.com>
2026-01-10 11:01:33 +0100

    added left & right wall

--- src/room/RoomMesh.jsx
+++ src/room/RoomMesh.jsx
@@ -5,6 +5,19 @@ const roomMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff" });
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
+export function FloorMesh({ width, depth, position }) {
+  return (
+    <RigidBody type="fixed" colliders="trimesh">
+      <mesh
+        geometry={planeGeometry}
+        material={roomMaterial}
+        rotation={[-Math.PI * 0.5, 0, 0]}
+        scale={[width, depth, 1]}
+      ></mesh>
+    </RigidBody>
+  );
+}
+
 export function BackWallMesh({ width, height, depth }) {
   return (
     <RigidBody type="fixed" colliders="trimesh">
@@ -18,15 +31,29 @@ export function BackWallMesh({ width, height, depth }) {
   );
 }
 
-export function FloorMesh({ width, depth, position }) {
+export function LeftWallMesh({ width, height, depth }) {
   return (
     <RigidBody type="fixed" colliders="trimesh">
       <mesh
         geometry={planeGeometry}
         material={roomMaterial}
-        rotation={[-Math.PI * 0.5, 0, 0]}
-        scale={[width, depth, 1]}
-        position={position}
+        scale={[depth, height, 1]}
+        rotation={[0, Math.PI * 0.5, 0]}
+        position={[-width * 0.5, height * 0.5, 0]}
+      ></mesh>
+    </RigidBody>
+  );
+}
+
+export function RightWallMesh({ width, height, depth }) {
+  return (
+    <RigidBody type="fixed" colliders="trimesh">
+      <mesh
+        geometry={planeGeometry}
+        material={roomMaterial}
+        scale={[depth, height, 1]}
+        rotation={[0, -Math.PI * 0.5, 0]}
+        position={[width * 0.5, height * 0.5, 0]}
       ></mesh>
     </RigidBody>
   );
@@ -35,8 +62,10 @@ export function FloorMesh({ width, depth, position }) {
 export default function RoomMesh({ size, position }) {
   return (
     <group position={position}>
+      <FloorMesh width={size[0]} depth={size[2]} />
       <BackWallMesh width={size[0]} height={size[1]} depth={size[2]} />
-      <FloorMesh width={size[0]} depth={size[2]} position={[0, 0, 0]} />
+      <LeftWallMesh width={size[0]} height={size[1]} depth={size[2]} />
+      <RightWallMesh width={size[0]} height={size[1]} depth={size[2]} />
     </group>
   );
 }
