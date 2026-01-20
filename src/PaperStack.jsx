9e18eebe0aad644f0a61f5594bc9ef150b1580a4
Karin Knutsson <karinmiriam@gmail.com>
2026-01-20 10:20:34 +0100

    created stack of papers

--- src/PaperStack.jsx
+++ src/PaperStack.jsx
@@ -1,3 +1,34 @@
+import { Text } from "@react-three/drei";
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(0.21, 0.297, 0.001);
+const paperMaterial = new THREE.MeshStandardMaterial({
+  color: "#ffffff",
+});
+
+export function PaperSheet({ position, rotation }) {
+  return (
+    <mesh
+      position={position}
+      rotation={rotation}
+      geometry={boxGeometry}
+      material={paperMaterial}
+      receiveShadow
+      castShadow
+    />
+  );
+}
+
 export default function PaperStack() {
-  return <></>;
+  return (
+    <>
+      {[...Array(20)].map((_, index) => (
+        <PaperSheet
+          key={index}
+          position={[0, index * 0.0014, 0]}
+          rotation={[Math.PI * 0.5, Math.random() * 0.001, 0]}
+        />
+      ))}
+    </>
+  );
 }
