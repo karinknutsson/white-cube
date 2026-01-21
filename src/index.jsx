82ebc80a216cea5360d21d2846d32158810ce966
Karin Knutsson <karinmiriam@gmail.com>
2026-01-21 15:12:37 +0100

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
