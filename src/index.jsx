c93c58b02e9bdd5332c01b7f0bce5656652e02f8
Karin Knutsson <karinmiriam@gmail.com>
2026-01-21 14:56:00 +0100

    removed unused usecontrols, removed aces tonemapping & set far camera to less

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,18 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-// import { Leva } from "leva";
 import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
   <>
-    {/* <Leva collapsed /> */}
     <Canvas
-      gl={{
-        toneMapping: THREE.ACESFilmicToneMapping,
-      }}
       shadows
       camera={{
         fov: 100,
         near: 0.1,
-        far: 200,
+        far: 20,
         position: [0, 2, 10],
       }}
       onCreated={({ gl }) => {
