1aa1012de074b0f07d79edf961d8f1520a025c61
Karin Knutsson <karinmiriam@gmail.com>
2026-02-09 18:58:20 +0100

    moving with arrow keys in float mode works

--- src/Player.jsx
+++ src/Player.jsx
@@ -7,11 +7,10 @@ import useGallery from "./stores/useGallery";
 
 export default function Player() {
   const { camera } = useThree();
+  const { isFloating } = useGallery();
   const bodyRef = useRef();
   const controlsRef = useRef();
   const cameraYOffset = useRef(0.7);
-
-  const { isFloating } = useGallery();
 
   const move = useRef({
     forward: false,
@@ -93,6 +92,9 @@ export default function Player() {
     };
   }, []);
 
+  /**
+   * Update player movement and camera position on each frame
+   */
   useFrame(() => {
     if (!bodyRef.current) return;
 
@@ -100,16 +102,21 @@ export default function Player() {
       const velocity = bodyRef.current.linvel();
       const direction = new THREE.Vector3();
 
+      // Set direction based on input
       if (move.current.forward) direction.z -= 1;
       if (move.current.backward) direction.z += 1;
       if (move.current.left) direction.x -= 1;
       if (move.current.right) direction.x += 1;
 
       direction.normalize();
+
+      // Rotate direction by camera orientation
       direction.applyEuler(camera.rotation);
 
+      // Set speed
       const speed = 1;
 
+      // Set velocity based on input direction and camera orientation
       bodyRef.current.setLinvel(
         {
           x: direction.x * speed,
@@ -119,6 +126,7 @@ export default function Player() {
         true,
       );
 
+      // Jump if player is on the ground and jump key is pressed
       if (move.current.jump && Math.abs(velocity.y) < 0.05) {
         bodyRef.current.setLinvel({ x: velocity.x, y: 4, z: velocity.z }, true);
       }
@@ -126,18 +134,64 @@ export default function Player() {
       const position = bodyRef.current.translation();
       const targetY = move.current.crouch ? position.y : position.y + 0.7;
 
+      // Set camera position based on body position and whether the player is crouching
       camera.position.y += (targetY - camera.position.y) * 0.1;
       camera.position.x = position.x;
       camera.position.z = position.z;
     } else {
+      const direction = new THREE.Vector3();
+
+      // Set direction based on input
+      if (move.current.forward) direction.z -= 1;
+      if (move.current.backward) direction.z += 1;
+      if (move.current.left) direction.x -= 1;
+      if (move.current.right) direction.x += 1;
+
+      direction.normalize();
+
+      const cameraDirection = new THREE.Vector3();
+      const cameraRight = new THREE.Vector3();
+
+      camera.getWorldDirection(cameraDirection);
+
+      cameraRight.crossVectors(camera.up, cameraDirection).normalize();
+
+      // Calculate total impulse
+      const impulse = new THREE.Vector3();
+      const impulseStrength = 0.05;
+
+      if (move.current.forward)
+        impulse.addScaledVector(cameraDirection, impulseStrength);
+      if (move.current.backward)
+        impulse.addScaledVector(cameraDirection, -impulseStrength);
+      if (move.current.left)
+        impulse.addScaledVector(cameraRight, impulseStrength);
+      if (move.current.right)
+        impulse.addScaledVector(cameraRight, -impulseStrength);
+
+      // Apply impulse
+      if (impulse.lengthSq() > 0) {
+        bodyRef.current.applyImpulse(impulse, true);
+      }
+
+      // bodyRef.current.applyImpulse(
+      //   {
+      //     x: direction.x * 0.5,
+      //     y: direction.y * 0.5,
+      //     z: direction.z * 0.5,
+      //   },
+      //   true,
+      // );
+
       const position = bodyRef.current.translation();
 
-      // choose target offset - center of body when floating, eye height when not
+      // Target offset:center of body when floating, eye height when not
       const targetOffset = isFloating ? 0 : 0.7;
 
-      // smooth the offset
+      // Smooth the offset
       cameraYOffset.current += (targetOffset - cameraYOffset.current) * 0.001;
 
+      // Set camera position based on body position and smoothed offset
       camera.position.y = position.y + cameraYOffset.current;
       camera.position.x = position.x;
       camera.position.z = position.z;
@@ -150,6 +204,7 @@ export default function Player() {
     if (isFloating) {
       // When the player starts floating, apply impulse to lift them up and into the room
       bodyRef.current.setEnabledRotations(true, true, true);
+      bodyRef.current.applyImpulse({ x: 0, y: 0.5, z: 0 }, true);
       bodyRef.current.applyTorqueImpulse({ x: 0.5, y: 0.5, z: 0 }, true);
     } else {
       // Reset velocity and rotation when player stops floating
