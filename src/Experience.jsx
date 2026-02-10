899c560bcb5b3813ba1050b8cc56d898fec9d11c
Karin Knutsson <karinmiriam@gmail.com>
2026-02-10 15:01:56 +0100

    added comments

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import { infoTextContent } from "./data/infoTextContent.js";
 
 export default function Experience() {
+  // Uncomment to keep track of performance
   // const perfVisible = true;
 
   // Set room dimensions
@@ -30,14 +31,19 @@ export default function Experience() {
 
   return (
     <>
+      {/* Uncomment to keep track of performance */}
       {/* {perfVisible && <Perf position="top-left" />} */}
 
       <Lights />
 
+      {/* Debug mode physics /*}
       {/* <Physics debug> */}
+
       <Physics>
+        {/* Environment map */}
         <Environment files="./hdr/qwantani_dusk_2_puresky_1k.hdr" background />
 
+        {/* Gallery */}
         <Gallery
           roomWidth={roomWidth}
           roomHeight={roomHeight}
@@ -45,6 +51,7 @@ export default function Experience() {
           wallThickness={wallThickness}
         />
 
+        {/* Player*/}
         <Player roomHeight={roomHeight} wallThickness={wallThickness} />
       </Physics>
     </>
