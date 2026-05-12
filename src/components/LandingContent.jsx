4a428593e485016094a105387a0f60584daf6165
Karin Knutsson <karinmiriam@gmail.com>
2026-05-12 17:49:12 +0200

    styled logo + backdrop

--- src/components/LandingContent.jsx
+++ src/components/LandingContent.jsx
@@ -4,8 +4,8 @@ import "../style.css";
 export default function LandingContent() {
   return (
     <div className="landing-container">
-      <div className="landing-content flex flex-col gap-4 m-60">
-        <h1>white cube</h1>
+      <div className="landing-content flex flex-col gap-6 m-60">
+        <h1 className="logo">white cube</h1>
         <p>An interactive 3D art gallery built with React Three Fiber.</p>
         <Button>Sign In</Button>
         <Button>View Demo</Button>
